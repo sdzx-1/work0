4f238b209eea705d325f71ec0252705f8609224a
EMQ-YangM <yangm@emqx.io>
2021-08-28 18:17:13 +0800

    interactive create graph

--- src/InteractiveGraph.hs
+++ src/InteractiveGraph.hs
@@ -18,6 +18,7 @@ import Control.Monad
 import Data.Either
 import Data.Graph.Inductive (Gr)
 import Data.Graph.Inductive.Dot
+import qualified Data.List as L
 import Graph (tmain)
 import System.Process
 import Text.Read
@@ -30,6 +31,20 @@ data ErrorType
 
 parse :: (Has (Error ErrorType) sig m, Read a) => String -> m a
 parse a = either (throwError . ArgsParseError . ((a ++ " ") ++)) return $ readEither a
+
+data Node a = Node
+  { nodeId :: Int,
+    sourceNodeId :: [Int],
+    code :: a
+  }
+  deriving (Show)
+
+helpCode :: [Int] -> String
+helpCode ls =
+  let k = case ls of
+        [] -> "input"
+        _ -> L.intercalate "," $ map (("node" ++) . show) ls
+   in "function handler(" ++ k ++ "){\n \n \n}"
 
 eval' ::
   ( Has (Lift IO :+: Error ErrorType) sig m,
@@ -45,12 +60,23 @@ eval' l = do
     "isEmpty" : _ -> isEmpty >>= sendIO . print
     "nodes" : _ -> nodes >>= sendIO . print
     "edges" : _ -> edges >>= sendIO . print
+    n : "<-" : ls -> do
+      nodeId <- parse n
+      insNode (nodeId, nodeId)
+      ls' <- mapM parse ls
+      forM_ (zip [1 ..] ls') $ \(i, s) -> do
+        insEdge (s, nodeId, i)
+      eval' "show"
+      sendIO $ do
+        writeFile ("work/m0/node" ++ show nodeId ++ ".txt") (helpCode ls')
+        void $ system ("gedit work/m0/node" ++ show nodeId ++ ".txt")
     ["+a", i] -> parse i >>= flip replicateM (fresh >>= \i -> insNode (i, i)) >> eval' "show"
     "+a" : _ -> fresh >>= \i -> insNode (i, i) >> eval' "show"
     ["+e", s, t] -> (,,1) <$> parse s <*> parse t >>= insEdge >> eval' "show"
     [s, "->", t, m] -> (,,) <$> parse s <*> parse t <*> parse m >>= insEdge >> eval' "show"
     [s, "->", t] -> (,,1) <$> parse s <*> parse t >>= insEdge >> eval' "show"
     "+e" : s : t : m : _ -> (,,) <$> parse s <*> parse t <*> parse m >>= insEdge >> eval' "show"
+    "file" : _ -> sendIO $ system "gedit work/ww.txt" >> print "edit finish"
     "finish" : _ -> throwError Finish
     _ -> sendIO $ print "unsport command"
 
