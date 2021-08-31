5ba1fee735db5a0a8bf30859002ff955f6ca70f1
EMQ-YangM <yangm@emqx.io>
2021-08-31 01:25:27 -0700

    finish select

--- src/MonomerGUI.hs
+++ src/MonomerGUI.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module MonomerGUI where
@@ -11,10 +12,55 @@ import Data.Default
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Typeable (cast)
+import Debug.Trace
 import Monomer
 import Monomer.Common.BasicTypes
 import qualified Monomer.Lens as L
 import Monomer.Widgets.Single
+
+data ICCanvas
+  = ICCricle
+      { _icPosition :: Point,
+        _icRadius :: Double
+      }
+  | ICArrow
+      { _icStartPoint :: Point,
+        _icEndPoint :: Point
+      }
+  deriving (Eq, Show)
+
+makeLenses ''ICCanvas
+
+inICCanvas :: Point -> ICCanvas -> Bool
+inICCanvas p ICCricle {..} =
+  (p ^. L.x - _icPosition ^. L.x) ^ 2
+    + (p ^. L.y - _icPosition ^. L.y) ^ 2 < _icRadius ^ 2
+inICCanvas p ICArrow {..} =
+  let x0 = p ^. L.x
+      x1 = _icStartPoint ^. L.x
+      x2 = _icEndPoint ^. L.x
+      y0 = p ^. L.y
+      y1 = _icStartPoint ^. L.y
+      y2 = _icEndPoint ^. L.y
+   in ((x0 - x1) * (y1 - y2) - (x1 - x2) * (y0 - y1)) ^ 2 / ((x1 - x2) ^ 2 + (y1 - y2) ^ 2) < 100 -- (power distancs)
+
+-- l =|(x0-x1)(y1-y2)-(x1-x2)(y0-y1)|/√[(x1-x2)^2+(y1-y2)^2]
+-- distance p0 to  line (p1,p2), this less 10
+
+data Stage
+  = InteractiveCanvas -- create node
+  | WriteHandlerFunction -- write handler function
+  deriving (Eq, Show)
+
+data CanvasState = CanvasState
+  { _icStage :: Stage,
+    _icCreatedNode :: [ICCanvas],
+    _icSelected :: [ICCanvas],
+    _icText :: Text
+  }
+  deriving (Eq, Show)
+
+makeLenses 'CanvasState
 
 newtype CanvasCfg = CanvasCfg
   { _canvasColors :: [Color]
@@ -40,13 +86,7 @@ data CanvasMessage
   = ResetCanvas
   deriving (Eq, Show)
 
-newtype CanvasState = CanvasState
-  { _clickedPoints :: [Point]
-  }
-  deriving (Eq, Show)
-
 makeLenses 'CanvasCfg
-makeLenses 'CanvasState
 
 canvasColor :: Color -> CanvasCfg
 canvasColor col = def & canvasColors .~ [col]
@@ -58,8 +98,17 @@ canvas_ :: [CanvasCfg] -> WidgetNode s e
 canvas_ configs = defaultWidgetNode "canvas" newWidget
   where
     config = mconcat configs
-    state = CanvasState []
+    state =
+      CanvasState
+        { _icStage = InteractiveCanvas,
+          _icCreatedNode = [],
+          _icSelected = [],
+          _icText = ""
+        }
     newWidget = makeCanvas config state
+
+globalRidaus :: Double
+globalRidaus = 30
 
 makeCanvas :: CanvasCfg -> CanvasState -> Widget s e
 makeCanvas cfg state = widget
@@ -91,13 +140,16 @@ makeCanvas cfg state = widget
       Click point button clicks -> Just result
         where
           newPoint = subPoint point origin
-          newPoints = newPoint : state ^. clickedPoints
-          newState = CanvasState newPoints
+          newState = state & icCreatedNode %~ (ICCricle newPoint globalRidaus :)
           newNode =
             node
               & L.widget .~ makeCanvas cfg newState
           result = resultNode newNode
-      Move _ -> Just (resultReqs node [RenderOnce])
+      Move p ->
+        let res =
+              let v = filter (inICCanvas (subPoint p origin)) (state ^. icCreatedNode)
+               in trace (show v) v
+         in Just $ resultNode $ node & L.widget .~ makeCanvas cfg (state & icSelected .~ res)
       _ -> Nothing
       where
         vp = node ^. L.info . L.viewport
@@ -106,7 +158,7 @@ makeCanvas cfg state = widget
     handleMessage wenv node target msg = case cast msg of
       Just ResetCanvas -> Just result
         where
-          newState = CanvasState []
+          newState = state & icCreatedNode .~ []
           newNode =
             node
               & L.widget .~ makeCanvas cfg newState
@@ -119,27 +171,37 @@ makeCanvas cfg state = widget
         sizeReqH = minHeight 100
 
     render wenv node renderer = do
-      drawInTranslation renderer origin $
-        forM_ tuples $ \(idx, pointA) -> do
+      drawInTranslation renderer origin $ do
+        forM_ tuples $ \(idx, icc) -> do
           setStrokeColor renderer (nextColor idx)
           setStrokeWidth renderer 2
           beginPath renderer
           -- renderLine renderer pointA pointB
           -- renderRect renderer (Rect (_pX pointA) (_pY pointA) 40 30)
-          renderText renderer pointA def (FontSize 10) def (T.pack $ show pointA)
-          renderArc renderer pointA 30 0 360 CW
+          -- renderText renderer (icc ^. icPosition) def (FontSize 10) def (T.pack $ show $ icc ^. icPosition)
+          case icc of
+            ICCricle {..} -> renderArc renderer _icPosition globalRidaus 0 360 CW
+            _ -> return ()
+          stroke renderer
 
+        forM_ (state ^. icSelected) $ \icc -> do
+          setStrokeColor renderer yellow
+          setStrokeWidth renderer 3
+          beginPath renderer
+          -- renderLine renderer pointA pointB
+          -- renderRect renderer (Rect (_pX pointA) (_pY pointA) 40 30)
+          -- renderText renderer (icc ^. icPosition) def (FontSize 10) def (T.pack $ show $ icc ^. icPosition)
+          case icc of
+            ICCricle {..} -> renderArc renderer _icPosition globalRidaus 0 360 CW
+            _ -> return ()
           stroke renderer
       where
         vp = node ^. L.info . L.viewport
         mousePos = wenv ^. L.inputStatus . L.mousePos
         newPoint = subPoint mousePos origin
         origin = Point (vp ^. L.x) (vp ^. L.y)
-        clicked = state ^. clickedPoints
-        points
-          | isPointInNodeVp node mousePos = reverse $ newPoint : clicked
-          | otherwise = reverse clicked
-        tuples = zip [0 ..] points
+        clicked = state ^. icCreatedNode -- [] -- state ^. clickedPoints
+        tuples = zip [0 ..] $ reverse clicked
 
 data AppModel
   = AppModel
